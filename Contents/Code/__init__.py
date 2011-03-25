b2babd170438d963720f2e7f93b3870d1ca29878
botho <sbothorel@free.fr>
2011-03-25 19:52:05 +0100

    V1.7 : ajout du picto cine-passion pour l'app store Plex online

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -254,7 +254,7 @@ class CinepassionAgent(Agent.Movies):
 				Log('[cine-passion Agent] : Content rating ('+ content_rating_source + ') is "'+ metadata.content_rating +'" for ' + metadata.title +' ('+ metadata.id +')')
 			
 			#collection
-			Log.Debug('[cine-passion Agent] : pref_ignore_collection  is "'+ str(Prefs["pref_ignore_collection"]) +'"')
+#			Log.Debug('[cine-passion Agent] : pref_ignore_collection  is "'+ str(Prefs["pref_ignore_collection"]) +'"')
 			metadata.collections.clear()
 			if Prefs["pref_ignore_collection"] == False:
 				if updateXMLresult.find('saga').text != None :
